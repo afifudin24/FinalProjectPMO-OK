4894e87125d47a1261068d9bbee91ab7664a1755
dzikririandri <dzikririandripratama@gmail.com>
2023-06-18 16:17:07 +0700

    DonasiService Fixes #47 Donasi Service Dzikri riandri

---
+++ lib/ClassService.dart/DonasiService.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../Class/DonasiClass.dart';
+
+class DonasiService {
+  final CollectionReference DonasiCollection =
+      FirebaseFirestore.instance.collection('donasi');
+
+  Future<List<Donasi>> getItems() async {
+    List<Donasi> donasiList = [];
+
+    QuerySnapshot snapshot = await DonasiCollection.get();
+
+    snapshot.docs.forEach((doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+      donasiList.add(Donasi.fromMap(data));
+    });
+    return donasiList;
+  }
+
+  Future<List<String>> getDocumentIds() async {
+    final querySnapshot = await DonasiCollection.get();
+    final documentIds = querySnapshot.docs.map((doc) => doc.id).toList();
+    return documentIds;
+  }
+
+  Future<void> addItem(Donasi item) {
+    String randomId = FirebaseFirestore.instance.collection('donasi').doc().id;
+    return DonasiCollection.doc(randomId).set(item.toMap());
+  }
+
+  Future<void> updateItem(Donasi item) {
+    return DonasiCollection.doc(item.namadonatur).update(item.toMap());
+  }
+
+  Future<void> deleteItem(String id) {
+    return DonasiCollection.doc(id).delete();
+  }
+}

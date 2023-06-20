316fd9fd60652be10925ab8c9342093c8f90c3b4
YeNiZhiao <afifrider507@gmail.com>
2023-06-20 15:57:46 +0700

    Proses Tambah Data | CRUD Barang Fixes #58

--- lib/Layout/BarangList.dart
+++ lib/Layout/BarangList.dart
@@ -25,7 +25,7 @@ class _KocakState extends State<Kocak> {
 
   Future<void> _loadUsers() async {
     print("lah");
-    List<Barang> barangs = await controller.getItems();
+    List<Barang> barangs = await controller.getItems("546");
     setState(() {
       _barang = barangs;
     });
@@ -39,89 +39,175 @@ class _KocakState extends State<Kocak> {
     });
     print(_id);
   }
+
   @override
+  TextEditingController conKodeBarang = TextEditingController();
+  TextEditingController conNamaBarang = TextEditingController();
+  TextEditingController conStokBarang = TextEditingController();
+  TextEditingController conHargaBarang = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("ok"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton.icon(onPressed: (){
-            showDialog(context: context, builder: (BuildContext context) {
-                  return AlertDialog(
-                        title: Text("uhuyy"),
-                        content: Text('uhuyy'),
-                        actions: [
-                          TextButton(onPressed: (){
-                            Navigator.pop(context);
-
-                          }, child: Text("OK"))
-                        ],
-                  );
-            });
-          }, icon: Icon(Icons.add), label: Text("Tambah")),
+        appBar: AppBar(
+          title: Text("Data Barang"),
         ),
-          Expanded(
-            child: ListView.builder(
-              
-              padding: EdgeInsets.all(8),
-             itemCount: _barang.length,
-             itemBuilder: (context, index) {
-             return DataTable(
-              decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                      ),
-              
-              columnSpacing: 10,
-                
-            columns: [
-                  
-              DataColumn(label: Text('NO')),
-              DataColumn(label: Text('Kode Barang')),
-              DataColumn(label: Text('Nama Barang')),
-              DataColumn(label: Text('Stok')),
-              DataColumn(label: Text('Harga')),
-              DataColumn(label: Text('Aksi')),
-              ],
-              rows: [
-               DataRow(
-          
-                cells: [
-                  DataCell(Text((index + 1) .toString(), textAlign: TextAlign.center,),),
-                  DataCell(Text(_barang[index].kdbarang)),
-                  DataCell(Text(_barang[index].namabarang)),
-                  DataCell(Text(_barang[index].stok.toString())),
-                  DataCell(Text(_barang[index].harga.toString())),
-                  DataCell(
-                    Row(children: [
-                      IconButton(
-                        onPressed: () {},
-                        color: Colors.amber,
-                        icon: Icon(Icons.visibility)),
-                      IconButton(
-                        onPressed: () {},
-                        color: Colors.green,
-                        icon: Icon(Icons.edit)),
-                      IconButton(
-                        onPressed: () {},
-                        color: Colors.red,
-                        icon: Icon(Icons.delete))
-                    ],)
-                  ),
-                ],
-              ),
-            ],
-              );
-              }),
-          ),
-        ],
-      ));
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text("Tambah Barang"),
+                              content: Container(
+                                child: Column(
+                                  children: [
+                                    TextField(
+                                      decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          label: Text("Kode Barang")),
+                                      controller: conKodeBarang,
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          label: Text("Nama Barang")),
+                                      controller: conNamaBarang,
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          label: Text("Harga Barang")),
+                                      controller: conHargaBarang,
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    TextField(
+                                      decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          label: Text("Stok Barang")),
+                                      controller: conStokBarang,
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          var kdbarang = conKodeBarang.text;
+                                          var namabarang = conNamaBarang.text;
+                                          int hargabarang =
+                                              int.parse(conHargaBarang.text);
+                                          int stokbarang =
+                                              int.parse(conStokBarang.text);
 
+                                          setState(() {
+                                            Barang newItem = Barang(
+                                                idtoko: '546',
+                                                kdbarang: kdbarang,
+                                                namabarang: namabarang,
+                                                stok: stokbarang,
+                                                harga: hargabarang);
+                                            controller.addItem(newItem);
+
+                                            _loadUsers();
+                                            refreshPage();
+                                          });
+                                        },
+                                        child: Text("Tambah Barang"))
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("OK"))
+                              ],
+                            );
+                          });
+                    },
+                    icon: Icon(Icons.add),
+                    label: Text("Tambah")),
+              ),
+              Expanded(
+                  child: Container(
+                child: Center(
+                  child: Column(
+                    children: [
+                      DataTable(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                          ),
+                          columnSpacing: 10,
+                          columns: [
+                            DataColumn(
+                                label: Text(
+                              'NO',
+                              textAlign: TextAlign.center,
+                            )),
+                            DataColumn(
+                                label: Text(
+                              'Kode Barang',
+                              textAlign: TextAlign.center,
+                            )),
+                            DataColumn(label: Text('Nama Barang')),
+                            DataColumn(label: Text('Stok')),
+                            DataColumn(label: Text('Harga')),
+                            DataColumn(label: Text('Aksi')),
+                          ],
+                          rows: List.generate(_barang.length, (index) {
+                            return DataRow(cells: [
+                              DataCell(
+                                Text(
+                                  (index + 1).toString(),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              DataCell(Text(_barang[index].kdbarang)),
+                              DataCell(Text(_barang[index].namabarang)),
+                              DataCell(Text(_barang[index].stok.toString())),
+                              DataCell(Text(_barang[index].harga.toString())),
+                              DataCell(Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      color: Colors.amber,
+                                      icon: Icon(Icons.visibility)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      color: Colors.green,
+                                      icon: Icon(Icons.edit)),
+                                  IconButton(
+                                      onPressed: () {},
+                                      color: Colors.red,
+                                      icon: Icon(Icons.delete))
+                                ],
+                              ))
+                            ]);
+                          })),
+                    ],
+                  ),
+                ),
+              ))
+            ]));
   }
+
+  void refreshPage() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => Kocak()),
+    );
   }
- 
+}
